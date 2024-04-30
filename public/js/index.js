ef1ee4597c36b196cfe1df33d2c56de2ca0c613c
Ali t.nazari <alitabatabaee20@gmail.com>
2024-04-30 22:12:43 +0330

    :lipstick: numbers now display in persian format

--- public/js/index.js
+++ public/js/index.js
@@ -7,3 +7,26 @@ if (closeButton != null) {
 		parent.remove();
 	});
 }
+
+function replaceEnglishWithPersianNumbers(inputString) {
+	const persianNumbers = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+
+	const englishNumbers = /[0-9]/g;
+
+	return inputString.replace(englishNumbers, function (match) {
+		return persianNumbers[parseInt(match)];
+	});
+}
+
+const coursePrices = Array.from(document.querySelectorAll('.course-price'));
+const courseTimes = Array.from(document.querySelectorAll('.course-time'));
+window.addEventListener('load', () => {
+	coursePrices.forEach(coursePrice => {
+		let price = coursePrice.dataset.price;
+		coursePrice.innerHTML = replaceEnglishWithPersianNumbers(price);
+	});
+	courseTimes.forEach(courseTime => {
+		let time = courseTime.dataset.time;
+		courseTime.innerHTML = replaceEnglishWithPersianNumbers(time);
+	});
+});

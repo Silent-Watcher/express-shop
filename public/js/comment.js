bdd82792aead973d64f87d876e471b643c06c86d
Ali t.nazari <alitabatabaee20@gmail.com>
2024-05-13 19:45:48 +0330

    :sparkles: loggedIn users can now reply to an existing comment

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,6 +1,9 @@
 const addNewCommentBtn = document.querySelector('#addNewCommentBtn');
 const newCommentSection = document.querySelector('#newCommentSection');
 const closeNewCommentSectionBtn = document.querySelector('#closeNewCommentSectionBtn');
+const replyCommentCTA = document.querySelector('#replyCommentCTA');
+const newReplySection = document.querySelector('#newReplySection');
+const closeNewReplySectionBtn = document.querySelector('#closeNewReplySectionBtn');
 
 addNewCommentBtn.addEventListener('click', () => {
 	if (newCommentSection.hidden) newCommentSection.hidden = false;
@@ -8,4 +11,12 @@ addNewCommentBtn.addEventListener('click', () => {
 
 closeNewCommentSectionBtn.addEventListener('click', () => {
 	newCommentSection.hidden = true;
+});
+
+replyCommentCTA.addEventListener('click', () => {
+	if (newReplySection.hidden) newReplySection.hidden = false;
+});
+
+closeNewReplySectionBtn.addEventListener('click', () => {
+	newReplySection.hidden = true;
 });
